4a26e175635d745b7c37035df4aa06891adef678
Bastian Stark <53190965+bastian-stark@users.noreply.github.com>
2023-12-15 17:14:32 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+#Band Name Generator
+
+#Intro
 print('Welcome to the band name generator')
+#Input City
 city = input('What is the name of the city you grew up in?\n')
+#Input pet Name
 pet = input('What is the name of your first pet?\n')
+#Concatenate strings
 band_name = city + ' ' + pet
-print(f'Your band name would be: {band_name})
+#Output band name
+print(f'Your band name would be: {band_name}')
